09a3e91da207a16b2d686346dbdd1e4c184361e2
jayuth <teerapong.uthairat.1877@gmail.com>
2019-07-18 10:57:18 -0400

    add a new solution

---
+++ csharp-solutions/HalloweenSale/HalloweenSale/Program.cs
@@ -0,0 +1,35 @@
+﻿// Link to this problem: https://www.hackerrank.com/challenges/halloween-sale/problem
+using System;
+
+namespace HalloweenSale
+{
+    class Solution
+    {
+        public static int HowManyGames(int p, int d, int m, int s)
+        {
+            var subsequence = p;
+            var sum = p;
+            var count = 0;
+
+            while (sum <= s)
+            {
+                subsequence -= d;
+
+                if (subsequence < m) subsequence = m;
+
+                sum += subsequence;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Solution.HowManyGames(100, 19, 1, 180);
+            Console.WriteLine(result);
+        }
+    }
+}
